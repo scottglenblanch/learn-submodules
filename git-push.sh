523a7f49b693ffe3bfa70d49b4db3079a1f46aa4
Scott Blanch <scottglenblanch@gmail.com>
2020-01-17 17:55:51 -0700

    updates

--- git-push.sh
+++ git-push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+cur="$(pwd)"
+git push --set-upstream origin $(git branch | grep \* | cut -d ' ' -f2-)
 
 for module in $(cat .gitmodules | grep path | sed  's/path = //g' | xargs echo)
 do
+  cd "${cur}/${module}"
   git push --set-upstream origin $(git branch | grep \* | cut -d ' ' -f2-)
 done
